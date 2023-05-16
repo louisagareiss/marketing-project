314f1b90cce04f5d1f5c31f83d0b38aa59f82088
Louisa Gareiss <82833707+louisagareiss@users.noreply.github.com>
2023-05-16 08:23:06 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+const cherryCursor = document.querySelector('.cherry-cursor');
+cherryCursor.style.cursor = 'url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\' width=\'20\' height=\'20\'><text y=\'16\' font-size=\'16\'>🍒</text></svg>") 10 10, auto';
